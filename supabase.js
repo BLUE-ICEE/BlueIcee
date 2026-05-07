57169731afe4d74672d228c8a6ead9004c0b24d7
BLUE-ICEE <isabellarivera2572@gmail.com>
2026-05-07 19:46:10 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,36 +1,12 @@
-const SUPABASE_URL =
+
+  const SUPABASE_URL =
   'https://qxplmvnwzvyjsqfwqbva.supabase.co'
 
 const SUPABASE_KEY =
   '/sb_publishable_i3SzWJgFFcwn_fOZRS0KBg_9CunDPNk'
 
-const supabaseClient =
+window.supabaseClient =
   supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
-  )
-create table messages (
-
-  id bigint generated always as identity primary key,
-
-  text text,
-
-  username text,
-
-  realm text,
-
-  created_at timestamptz default now()
-
-);
-
-alter table messages enable row level security;
-
-create policy "Allow read access"
-on messages
-for select
-using (true);
-
-create policy "Allow insert access"
-on messages
-for insert
-with check (true);
+  );
