c3b174935c4aba75e6db723e614fc662287a80e6
BLUE-ICEE <isabellarivera2572@gmail.com>
2026-05-07 18:40:58 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 const SUPABASE_URL =
-  'https://qxplmvnwzvyjsqfwqbva.supabase.co/rest/v1/'
+  'https://qxplmvnwzvyjsqfwqbva.supabase.co'
 
 const SUPABASE_KEY =
-  'sb_publishable_i3SzWJgFFcwn_fOZRS0KBg_9CunDPNk'
+  '/sb_publishable_i3SzWJgFFcwn_fOZRS0KBg_9CunDPNk'
 
 const supabaseClient =
   supabase.createClient(
